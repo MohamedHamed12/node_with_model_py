1d88bd4557e5616189e97a31ab418330a3784449
MohamedHamed12 <mh1779371@gmail.com>
2024-01-28 10:52:12 +0200

    return form model

--- app.js
+++ app.js
@@ -6,20 +6,14 @@ const app = express();
 const PORT = 3000;
 
 let options = {
-    args: [5] // Assuming the number is passed as a command-line argument
+    args: [5, 6] // Assuming the number is passed as a command-line argument
 };
+
 // run model.py
-ps.PythonShell.run('model.py', options, function (err, results) {
-    if (err) throw err;
-    console.log('results: %j', results);
+
+ps.PythonShell.run('model.py', options).then(messages=>{
+  console.log('results: %j', messages);
 });
-// ------------
-// read txt file 
-//----------
-const fs = require('fs');
-const data = fs.readFileSync('number.txt', 'utf8');
-console.log(data);
-//-------------
 
 app.get('/', (req, res) => {
     res.send('Hello World! res = ' + res);
--- model.py
+++ model.py
@@ -4,13 +4,10 @@ import sys
 
 
 def calculate_square(number):
-    # make any calculation here
-    
-    # make file with num 
-    with open("number.txt", "w") as file:
-        file.write(str(number))
-    return int(number) * int(number)
+    print(98)
+
 
 if __name__ == "__main__":
-    number = sys.argv[1]
-    result = calculate_square(number)
+    number1 = sys.argv[1]
+    number2= sys.argv[2]
+    result = calculate_square(number1)
